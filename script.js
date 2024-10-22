8b42a9db09b84a6e2023791449d8c2e3023930ca
Shubik Bhatt <shubikbhatt2h7h@gmail.com>
2024-10-22 20:08:57 +1100

    learnt to declare, initialize and assign a variable

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log("Hello!!")
+//This is a  single line Comment.\
+
+/*
+
+This is 
+a 
+multi-line comment
+
+*/
+
+
+console.log("Hello!!") 
+
+
+// Lets learn VARIABLE
+//  three ways to initialize a variable 
+//  var, let and const
+
+
+var name // declaring a variable
+var name ="Shubik Bhatt"  // assignig variable
+ 
+
+let add = "Sydney"
+console.log(name,  add);
+
+
+name ="sam"
+console.log(name)  // Changes the value of name from Shubik Bhatt to Sam ( Overwriting Data)
+
+const dob = "20/12/2000";
+console.log(dob);
+
+
+
+
+
