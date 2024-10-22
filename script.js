62fe4314dbe021ef6770cef26edda045fa305942
Shubik Bhatt <shubikbhatt2h7h@gmail.com>
2024-10-22 20:32:47 +1100

    Learnt Arthemitic operartions on variables

--- script.js
+++ script.js
@@ -32,6 +32,46 @@ const dob = "20/12/2000";
 console.log(dob);
 
 
+// Lets Learn Arthemitics
+
+/*
+
++	Addition
+ const ans = a + b;
+-	Subtraction
+*	Multiplication
+ **	Exponentiation (ES2016)
+ /	Division
+%	Modulus (Division Remainder)
+++	Increment
+--	Decrement
+
+*/
+
+let a = 5
+let b = 10
+let c = b
+
+const sum  = a + b + c  // 5 + 6 + 6
+const Subtraction = b - a
+const multi = a*b
+const expo = b ** a
+const modolus = b % a 
+
+a ++; // increments from 5 to 6  also known as postfix increment
+
+b--;
 
 
+console.log("The Sum is  " + sum + ".")
+console.log("The difference of " + b + " and " + a + " is " + Subtraction + ".")
+console.log("The product is  " + multi + ".")
+console.log("The exponential  is  " + expo + ".")
+console.log("The remainder  is  " + modolus + ".")
+console.log("One increment is  " + a)
+console.log( " The Decrement is  " + b)
+
+console.log(++a) // using a prefix incrmement
+
+console.log( 4+5 ) // can perform arthemetics
 
