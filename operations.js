36a1c7fefd9a18220ca68546a7a47c8fdf19c79f
Shubik Bhatt <shubikbhatt2h7h@gmail.com>
2024-10-29 16:12:40 +1100

    test

---
+++ operations.js
@@ -0,0 +1,21 @@
+// wap to find the sum , difference , product and quotient of given two numbers
+
+//Binary Operations
+
+// JS was built in 10 days and it takes 10 years to master
+let num1 = 10;
+let  num2 = 20;
+
+let sum = num1 + num2;
+let difference = num2 - num1;
+let product = num1 * num2;
+let quotient = num2 / num1;
+
+console.log("The two numbers are " + num1 + " and " + num2 + "." );
+
+console.log("And the sum, difference, product and Quotient are " + sum + ", " + difference + ", " +  product + " and " + quotient + " respectively." );
+
+
+
+
+
---
+++ variables.js
@@ -0,0 +1,25 @@
+// create all the types of the variables
+
+// string
+const fName = " Shubik";
+let lName = "Bhatt"
+
+console.log(fName + " " + lName);
+
+// number
+let num1 = 10;
+let num2 = 5;
+
+console.log(num1 + " " + num2 + " are your numbers");
+
+//Boolean
+
+isMale = true;
+isMarried = false;
+
+console.log("He is a Male: " + isMale);
+console.log("He is Married: " + isMarried);
+
+
+
+
