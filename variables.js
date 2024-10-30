a810b4f64fd9d4f4893d0a539fc826d63aba05c7
Shubik Bhatt <shubikbhatt2h7h@gmail.com>
2024-10-30 20:12:19 +1100

    Conversion Operators

--- variables.js
+++ variables.js
@@ -1,16 +1,30 @@
-// create all the types of the variables
+// create variables to store all the types of  Data types.
+
+
+//  primitive Data Types:
 
 // string
 const fName = " Shubik";
 let lName = "Bhatt"
 
 console.log(fName + " " + lName);
+console.log(typeof fName);
+
 
 // number
 let num1 = 10;
 let num2 = 5;
 
 console.log(num1 + " " + num2 + " are your numbers");
+
+console.log(typeof num1);
+
+// bigInt
+
+let bigNum = 123456732123456721n;
+console.log(bigNum);
+
+console.log(typeof bigNum);
 
 //Boolean
 
@@ -19,6 +33,33 @@ isMarried = false;
 
 console.log("He is a Male: " + isMale);
 console.log("He is Married: " + isMarried);
+
+console.log(typeof isMale);
+
+
+
+// null
+let qtyOfApples = null;
+
+console.log(typeof qtyOfApples);
+
+
+
+// symbol
+
+
+// Undefined:
+
+let children;
+
+console.log(children);
+
+console.log(typeof children);
+
+
+
+
+
 
 
 
